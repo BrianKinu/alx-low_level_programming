ab326ddcdc93fe28da46b7bb0921fa9193d62b59
BrianKinu <briankinuthia59@gmail.com>
2023-02-16 08:25:41 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,13 @@
 /**
  * main * prints "Programing is like building a multilingual 
  * puzzle followed by a newline.
- * Return:Always 0.*/
+ * Return:Always 0.
+ */
 
 
 int main(void)
 {
-	puts("\'Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
