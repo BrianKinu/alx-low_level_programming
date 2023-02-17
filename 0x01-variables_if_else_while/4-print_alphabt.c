4ea0ecd624565d3b733986642cac4d6f51c16c30
BrianKinu <briankinuthia59@gmail.com>
2023-02-17 04:29:38 -0800

    e, q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_alphabr.c
+ * Auth: Brian
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowecase, except for a q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
