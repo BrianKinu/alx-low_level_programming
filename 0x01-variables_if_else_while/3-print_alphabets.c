ee90ca117414bb7f7c517bfd2670252bc25846e8
BrianKinu <briankinuthia59@gmail.com>
2023-02-17 04:21:39 -0800

    upper alphs

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/*
+ * File:3-prints_alphabets.c
+ * Auth: Brian
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then uppercase.
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
