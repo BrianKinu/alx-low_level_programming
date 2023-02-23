b20d56c4d4a1a224db60181d0bdd8ff79ba86f10
BrianKinu <briankinuthia59@gmail.com>
2023-02-23 04:45:36 -0800

    adds two intergers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,7 +7,7 @@
 /**
   * mul - Multiplies two integers
   * @a: The first number to multiply
-  * b: The second number to multiply with first number
+  * @b: The second number to multiply with first number
   *
   * Return: The result of multiplication between a param and b param
   */
