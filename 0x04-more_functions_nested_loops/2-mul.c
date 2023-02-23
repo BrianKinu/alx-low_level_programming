27a80ccab505f1fcbd0e19ea991ad1e9321e1b05
BrianKinu <briankinuthia59@gmail.com>
2023-02-23 04:19:22 -0800

    adss two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
   * mul - Multiplies two integers
-  * @x: The first number to multiply
-  * @y: The second number to multiply with first number
+  * @a: The first number to multiply
+  * b: The second number to multiply with first number
   *
   * Return: The result of multiplication between a param and b param
   */
