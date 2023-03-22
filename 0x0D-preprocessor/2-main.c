581bf16faa704d71163810f5160f7a1cb795e008
BrianKin <briankinuthia58@gmail.com>
2023-03-22 14:53:05 +0300

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,22 +1,13 @@
+#include "1-pi.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main(void)
 {
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
