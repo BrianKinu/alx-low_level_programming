4783af547d624168c6ea1b57b5888a3441cb0d0d
BrianKin <briankinuthia58@gmail.com>
2023-03-25 10:58:20 +0300

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,16 @@
-#include "function_pointers.h"
+#include "holberton.h"
 
 /**
- * print_name - print a name using the function supplied by the caller
- * @name: the name to print
- * @f: the function to call to print the name
+ * print_name - prints a name.
+ * @name: string to print.
+ * @f: fuction to use.
  *
+ * Return: NOTHING
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
+	if (!f || !name)
 		return;
 	f(name);
 }
+
