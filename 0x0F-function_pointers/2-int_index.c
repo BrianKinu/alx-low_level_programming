cb62e10c99dfb6a2c352c65244f58781f1b6234f
BrianKin <brainkinuthia58@gmail.com>
2023-03-29 15:25:42 +0300

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
 #include "function_pointers.h"
-/**
- * array_iterator - function that executes
- * a function given as a parameter
- * on each element of an array
- *
- * @array: array with the parameters
- * @size: size of the array
- * @action: function as a parameter
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	size_t i;
 
-	if (array == NULL || action == NULL)
-	{
-		return;
-	}
+/**
+ * int_index - Function that searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of first element that matches with `cmp`, or -1 if none found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		(*action)(array[i]);
+		if (cmp(array[i]))
+			return (i);
 	}
+
+	return (-1);
+}}
 }
